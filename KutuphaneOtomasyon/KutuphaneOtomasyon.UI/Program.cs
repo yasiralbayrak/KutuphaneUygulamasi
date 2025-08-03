1e4e0c240cda148750a77cb6882478262a8bcf23
yasiralbayrak <yasiralbayrak04@gmail.com>
2025-08-03 20:09:16 +0300

    Add files via upload

---
+++ KutuphaneOtomasyon/KutuphaneOtomasyon.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KutuphaneOtomasyon.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
